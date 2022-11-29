098b0bb1c901fc55a4b4db106697e5ae4065dec0
Hasan Balcı <balcihasan99@gmail.com>
2022-11-29 17:35:34 +0300

    Add filter UI to demo

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', onLoaded);
 
-
-
 function onLoaded() {
   let instance;
   const cyVisible = window.cyVisible = cytoscape({
@@ -57,7 +55,6 @@ function onLoaded() {
   const cy = window.cy = cytoscape({
     ready: function(){
       instance = this.complexityManagement();
-      initailizer(this);
     },
     container: document.getElementById('cy'),
     wheelSensitivity: 0.1,
@@ -107,7 +104,7 @@ function onLoaded() {
         { data: { id: 'f-g', source: 'f', target: 'g' } }
       ]
     },
-    layout: { name: 'fcose', animate: false}
+    layout: { name: 'fcose', animate: true, stop:function(){initailizer(cy);}}
   });
 
   cy.layoutUtilities({ desiredAspectRatio: cy.width() / cy.height() });
@@ -118,22 +115,22 @@ function onLoaded() {
   function initailizer(cy){
     cyVisible.remove(cyVisible.elements());
     cyInvisible.remove(cyInvisible.elements());
+
     instance.getCompMgrInstance().visibleGraphManager.nodesMap.forEach((nodeItem,key) => {
-      cyVisible.add({data:{id: nodeItem.ID,parent:instance.getCompMgrInstance().visibleGraphManager.rootGraph === nodeItem.owner ? null : nodeItem.owner.parent.ID}, position:cy.getElementById(nodeItem.ID).position()});
+      cyVisible.add({data: {id: nodeItem.ID, parent: instance.getCompMgrInstance().visibleGraphManager.rootGraph === nodeItem.owner ? null : nodeItem.owner.parent.ID}, position: cy.getElementById(nodeItem.ID).position()});
     });
-    
     instance.getCompMgrInstance().visibleGraphManager.edgesMap.forEach((edgeItem,key) => {
-      cyVisible.add({data:{id: edgeItem.ID,source:edgeItem.source.ID,target:edgeItem.target.ID}});
+      cyVisible.add({data: {id: edgeItem.ID, source: edgeItem.source.ID, target: edgeItem.target.ID}});
     });
-    cyVisible.fit(cyVisible.elements(),30);
+    cyVisible.fit(cyVisible.elements(), 30);
 
     instance.getCompMgrInstance().invisibleGraphManager.nodesMap.forEach((nodeItem,key) => {
-      cyInvisible.add({data:{id:nodeItem.ID ,label: nodeItem.ID + (nodeItem.isFiltered?"(f)":"") + (nodeItem.isHidden?"(h)":"") + (nodeItem.isCollapsed?"(c)":"") + (nodeItem.isVisible?"":"(i)"),parent:instance.getCompMgrInstance().visibleGraphManager.rootGraph === nodeItem.owner ? null : nodeItem.owner.parent.ID}, position:cy.getElementById(nodeItem.ID).position()});
+      cyInvisible.add({data: {id: nodeItem.ID, label: nodeItem.ID + (nodeItem.isFiltered ? "(f)" : "") + (nodeItem.isHidden ? "(h)" : "") + (nodeItem.isCollapsed ? "(c)" : "") + (nodeItem.isVisible ? "" : "(i)"), parent: instance.getCompMgrInstance().visibleGraphManager.rootGraph === nodeItem.owner ? null : nodeItem.owner.parent.ID}, position: cy.getElementById(nodeItem.ID).position()});
     });
     instance.getCompMgrInstance().invisibleGraphManager.edgesMap.forEach((edgeItem,key) => {
-      cyInvisible.add({data:{id: edgeItem.ID, label: edgeItem.ID + (edgeItem.isFiltered?"(f)":"") + (edgeItem.isHidden?"(h)":"") + (edgeItem.isVisible?"":"(i)"),source:edgeItem.source.ID,target:edgeItem.target.ID}});
+      cyInvisible.add({data: {id: edgeItem.ID, label: edgeItem.ID + (edgeItem.isFiltered ? "(f)" : "") + (edgeItem.isHidden ? "(h)" : "") + (edgeItem.isVisible ? "" : "(i)"),source: edgeItem.source.ID, target: edgeItem.target.ID}});
     });
-    cyInvisible.fit(cyInvisible.elements(),30);
+    cyInvisible.fit(cyInvisible.elements(), 30);
   }
 
   document.getElementById("addNodeToSelected").addEventListener("click", () => {
@@ -146,15 +143,17 @@ function onLoaded() {
     }
     cy.add({
       group: 'nodes',
-      data: { id: 'newNode' + newNodeCount, parent: parentID},
+      data: { id: 'nn' + newNodeCount, parent: parentID},
       position: position
     });
     newNodeCount++;
 
     if (document.getElementById("cbk-run-layout2").checked) {
-      cy.layout({name: "fcose", animate: false, randomize: false}).run();
+      cy.layout({name: "fcose", animate: true, randomize: false, stop: () => {initailizer(cy)}}).run();
     }
-    initailizer(cy);
+    else {
+      initailizer(cy);
+    }
   });
 
   document.getElementById("addEdgeBetweenSelected").addEventListener("click", () => {
@@ -167,21 +166,94 @@ function onLoaded() {
     newEdgeCount++;
 
     if (document.getElementById("cbk-run-layout2").checked) {
-      cy.layout({name: "fcose", animate: false, randomize: false}).run();
+      cy.layout({name: "fcose", animate: true, randomize: false, stop: () => {initailizer(cy)}}).run();
     }
-    initailizer(cy);
+    else {
+      initailizer(cy);
+    }
   });
 
   document.getElementById("removeSelected").addEventListener("click", () => {
     cy.elements(":selected").remove();
 
     if (document.getElementById("cbk-run-layout2").checked) {
-      cy.layout({name: "fcose", animate: false, randomize: false}).run();
+      cy.layout({name: "fcose", animate: true, randomize: false, stop: () => {initailizer(cy)}}).run();
     }
-    initailizer(cy);
+    else {
+      initailizer(cy);
+    }
   });
 
+  // Slider operations
+  $("#slider-nodes").slider({
+    range: true,
+    min: 0,
+    max: 100,
+    step: 1,
+    values: [0, 100],
+    slide: function(event, ui) {
+      let delay = function() {
+        let handleIndex = ui.handleIndex;
+        let label = handleIndex == 0 ? '#min-weight-node' : '#max-weight-node';
+        $(label).html(ui.value).position({
+            my: 'center top',
+            at: 'center bottom',
+            of: ui.handle,
+            offset: "0, 10"
+        });
+      };
+      // wait for the ui.handle to set its position
+      setTimeout(delay, 5);
+    }
+  });
 
-  
+  $('#min-weight-node').html($('#slider-nodes').slider('values', 0)).position({
+    my: 'center top',
+    at: 'center bottom',
+    of: $('#slider-nodes span:eq(0)'),
+    offset: "0, 0"
+  });
 
+  $('#max-weight-node').html($('#slider-nodes').slider('values', 1)).position({
+    my: 'center top',
+    at: 'center bottom',
+    of: $('#slider-nodes span:eq(1)'),
+    offset: "0, 10"
+  });
+
+  $("#slider-edges").slider({
+    range: true,
+    min: 0,
+    max: 100,
+    step: 1,
+    values: [0, 100],
+    slide: function(event, ui) {
+      let delay = function() {
+        let handleIndex = ui.handleIndex;
+        let label = handleIndex == 0 ? '#min-weight-edge' : '#max-weight-edge';
+        $(label).html(ui.value).position({
+            my: 'center top',
+            at: 'center bottom',
+            of: ui.handle,
+            offset: "0, 10"
+        });
+      };
+      // wait for the ui.handle to set its position
+      setTimeout(delay, 5);
+    }
+  });
+
+  $('#min-weight-edge').html($('#slider-edges').slider('values', 0)).position({
+    my: 'center top',
+    at: 'center bottom',
+    of: $('#slider-edges span:eq(0)'),
+    offset: "0, 0"
+  });
+
+  $('#max-weight-edge').html($('#slider-edges').slider('values', 1)).position({
+    my: 'center top',
+    at: 'center bottom',
+    of: $('#slider-edges span:eq(1)'),
+    offset: "0, 10"
+  });  
 }
