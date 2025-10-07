0e68ecb741a9d0968aecc843b1b2deca760b8b07
Osama Zafar <49822311+osamazafar980@users.noreply.github.com>
2025-10-07 16:50:55 -0400

    Update publish

--- src/index.js
+++ src/index.js
@@ -40,6 +40,88 @@ export default function register(cytoscape) {	// register with cytoscape.js
     return getScratch(cy, 'api');
   });
   
+  const cyLayout = window.cyLayout =  cytoscape({
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'label': (node) => {
+            return node.data('label');
+          },
+          "color" : "black",
+          'font-size': '20px',
+          'compound-sizing-wrt-labels': 'include',
+          'height': 40,
+          'width': 40,
+          'padding': "5px",
+          "background-fit": "cover",
+          "border-color": "black",
+          "border-width": 1,
+          "border-opacity": 1,
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'label': (edge) => {
+
+            return edge.data('weight');
+          },
+          'curve-style': 'bezier',
+          'target-arrow-shape': 'triangle',
+          'text-rotation': 'autorotate',
+          'width': '1.5px',
+          'text-margin-y': '10px',
+          'line-color' : 'black',
+          'target-arrow-color': 'black',
+        }
+      }],
+    headless:true,
+    styleEnabled:true
+  });
+  const cyVisible = window.cyVisible = cytoscape({
+    headless:true,
+    styleEnabled:true,
+    wheelSensitivity: 0.1,
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'label': 'data(id)',
+          "color" : "black",
+          'font-size': '20px',
+          'compound-sizing-wrt-labels': 'include',
+          'height': 40,
+          'width': 40,
+          'padding': "5px",
+          "background-fit": "cover",
+          "border-color": "black",
+          "border-width": 1.5,
+          "border-opacity": 1,
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'label': (x) => {
+            if (x.data('edgeType')) {
+              return x.data('edgeType');
+            }
+            return '';
+          },
+          'curve-style': 'bezier',
+          'target-arrow-shape': 'triangle',
+          'text-rotation': 'autorotate',
+          'font-size': '14px',
+          'width': "1.5px",
+          'text-margin-y': '10px',
+          'line-color' : 'black',
+          'target-arrow-color': 'black',
+          
+        }
+      }
+    ]
+  });
   // Get the whole scratchpad reserved for this extension (on an element or core) or get a single property of it
   function getScratch(cyOrEle, name) {
     if (cyOrEle.scratch('cyComplexityManagement') === undefined) {
